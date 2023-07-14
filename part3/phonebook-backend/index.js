227908c0d8e3c7711956b666d4dc87370d32aaff
soken <nsongkien@gmail.com>
2023-07-14 09:29:56 +0300

    3.3

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app=express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/',(request,response)=>{
+    response.send('<h1>API implementation for phonebook</h1>')
+})
+
+app.get('/info',(request,response)=>{
+    response.end(
+    `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date}</p>
+    </div>`)
+})
+
+app.get('/api/persons',(request,response)=>{
+    response.json(persons)
+    console.log(persons);
+})
+
+app.get('/api/persons/:id',(request,response)=>{
+    const id=Number(request.params.id)
+    const person=persons.find(person=>person.id===id)
+    
+    if (person){
+        response.json(person)
+        console.log(person)
+    }   
+    else {
+        console.log('404 Not Found')
+        response.status(404).end() 
+    }
+})
+
+
+
+
+const PORT = 3001
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
