304d5c6536fed61cfdfdef97d70a347a4e7536b4
soken <nsongkien@gmail.com>
2023-07-15 09:31:35 +0300

    morgan

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app=express()
 app.use(express.json())
+
+
+morgan.token('body', (req)=> { return JSON.stringify(req.body)})
+app.use(morgan(`:method :url :status - :response-time ms :body`))
+
+
 let persons = [
     { 
       "id": 1,
