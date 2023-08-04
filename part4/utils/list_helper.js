3715a2271804f6e5de74ea75b0b3f9a15d815215
soken <nsongkien@gmail.com>
2023-08-04 21:49:40 +0300

    4.7 | missing lint

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -9,8 +11,24 @@ const totalLikes = (blogs) =>{
     return blogs.map(blog=>blog.likes).reduce(reducer ,0)
 }
 
+const mostLikes = (blogs) => {
+    let countAuthorLike = _(blogs)
+        .groupBy('author')
+        .map((authors, id)=>({
+            author: id,
+            likes: _.sumBy(authors,'likes')
+        }))
+        .value()
+        
+        countAuthorLike = _.maxBy(countAuthorLike,(object)=>(object.likes))
+
+    console.log(countAuthorLike)
+    return countAuthorLike
+}
+
 module.exports={
     dummy,
-    totalLikes
+    totalLikes,
+    mostLikes,
 }
 
