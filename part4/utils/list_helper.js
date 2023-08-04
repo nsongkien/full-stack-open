4daa84291d1d1b3d1bb5e8c904c85674817bc348
soken <nsongkien@gmail.com>
2023-08-04 21:49:47 +0300

    4.7 | missing lint

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -11,6 +11,21 @@ const totalLikes = (blogs) =>{
     return blogs.map(blog=>blog.likes).reduce(reducer ,0)
 }
 
+const mostBlogs = (blogs) => {
+    let countAuthorBlogs = _(blogs)
+        .groupBy('author')
+        .map((authors, id)=>({
+            author: id,
+            blogs: authors.length
+        }))
+        .value()
+
+        countAuthorBlogs = _.maxBy(countAuthorBlogs,(object)=>(object.blogs))
+
+    console.log(countAuthorBlogs)
+    return countAuthorBlogs
+}
+
 const mostLikes = (blogs) => {
     let countAuthorLike = _(blogs)
         .groupBy('author')
@@ -19,16 +34,16 @@ const mostLikes = (blogs) => {
             likes: _.sumBy(authors,'likes')
         }))
         .value()
-        
+
         countAuthorLike = _.maxBy(countAuthorLike,(object)=>(object.likes))
 
-    console.log(countAuthorLike)
     return countAuthorLike
 }
 
 module.exports={
     dummy,
     totalLikes,
+    mostBlogs,
     mostLikes,
 }
 
