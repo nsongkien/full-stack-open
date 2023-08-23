d5224c321168ba44266b2670d6b2a594676ce712
soken <nsongkien@gmail.com>
2023-08-23 08:43:09 +0300

    5.4

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -2,18 +2,32 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import SuccessNotification from './components/SuccessNotification'
+import ErrorNotification from './components/ErrorNotification'
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [errorMessage, setErrorMessage] = useState('')
+  const [title,setTitle]= useState('')
+  const [author,setAuthor]=useState('')
+  const [url, setUrl] = useState('')
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const renderBlogList = () => (
@@ -22,7 +36,27 @@ const App = () => {
     )
   )
 
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const addedBlog = await blogService.create({title, author, url})
+    if (addedBlog !== null) {
+          setSuccessMessage(`Added: ${addedBlog.title}`)
+          setAuthor('')
+          setTitle('')
+          setUrl('')
+          setBlogs(blogs.concat(addedBlog))
+    }
+    else setErrorMessage(`Error, blog could not be added`)
+
+    setTimeout(() => {
+      setSuccessMessage(null)
+      setErrorMessage(null)
+    }, 5000);
+  }
+
   const loginForm = ()=> (
+    <>
+    Log into your account
     <form onSubmit={handleLogin}>
       <div>
         username
@@ -44,6 +78,43 @@ const App = () => {
       </div>
       <button type='submit'>Log in</button>
     </form>
+    </> 
+  )
+
+  const newBlogForm = () => (
+    <>
+      Create A Post:
+      <form onSubmit={addBlog}>
+        <div>
+          Title: 
+          <input
+            type="text" 
+            value={title}
+            name="Title"
+            onChange={({target})=> setTitle(target.value)}
+          />
+        </div>
+        <div>
+          Author: 
+          <input
+            type="text"
+            value={author}
+            name='Author'
+            onChange={({target})=> setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          Url: 
+          <input
+            type="text"
+            value={url}
+            name='Url'
+            onChange={({target})=>setUrl(target.value)}
+          />
+        </div>
+        <button type='submit'>Post</button>
+      </form>
+    </>
   )
 
   const handleLogin = async (event) => {
@@ -52,29 +123,49 @@ const App = () => {
     try {
       const user = await loginService.login({username,password})
       blogService.setToken(user.token)
+      
       setUser(user)
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
       setUsername('')
       setPassword('')
     }
     catch (exceptions) {
-      setErrorMessage('Wrong Credentials')
-      console.log('wrong credentials');
+      setErrorMessage('Wrong Username or Password')
+      console.log('wrong credentials')
       setTimeout(() => {
-        setErrorMessage('')
+        setErrorMessage(null)
       }, 5000);
     }
-    
+  }
+
+  const handleLogout = async () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    location.reload()
   }
 
   return (
     <div>
-      <h2>blogs</h2>
+      <h1>Favorite Blogs</h1>
+      <SuccessNotification successMessage={successMessage}/>
+      <ErrorNotification errorMessage={errorMessage}/>
+
       {
-        user === null
+        !user
           ? loginForm()
           : <div>
-              <p>{user.name} logged in</p>
+              <p>
+                {user.name} logged in
+                <button onClick={handleLogout}>log out</button>
+              </p>
+
+              {newBlogForm()}
+
+              
               {renderBlogList()}
+              
             </div>
               
             
